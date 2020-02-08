ba56857628a4be5240d31fcd7c69ab7468eee980
Depasse Arthur <arthur.depasse96@gmail.com>
2020-02-08 16:20:04 +0000

    initial backend commit

---
+++ backend/swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.receipt import Receipt
+from swagger_server.models.receipt_item import ReceiptItem

f78c0e6a5ebfe451a542c101f0e2511e87d029fb
Depasse Arthur <arthur.depasse96@gmail.com>
2020-02-08 16:56:36 +0000

    adding user endpoint

--- backend/swagger_server/models/__init__.py
+++ backend/swagger_server/models/__init__.py
@@ -5,3 +5,4 @@ from __future__ import absolute_import
 # import models into model package
 from swagger_server.models.receipt import Receipt
 from swagger_server.models.receipt_item import ReceiptItem
+from swagger_server.models.user import User

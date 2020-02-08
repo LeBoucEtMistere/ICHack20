345f6c82940c0150e3fa9097c5d259c32a639fa0
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-02-08 22:43:47 +0000

    Removed app folder

--- app/jest.setup.js
+++
@@ -1,8 +0,0 @@
-import { NativeModules } from 'react-native';
-
-NativeModules.RNCNetInfo = {
-  getCurrentConnectivity: jest.fn(),
-  isConnectedMetered: jest.fn(),
-  addListener: jest.fn(),
-  removeListeners: jest.fn(),
-};
--- app/src/components/Title.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const Title = () => {
-  return <Text h1>VegEarly</Text>;
-};
-
-export default Title;

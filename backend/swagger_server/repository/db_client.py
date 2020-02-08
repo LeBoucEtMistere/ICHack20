2e2193e732447d2c94b9ba1ad2ea15e6c9f83ce3
ryzzaki <cuong.nugi@gmail.com>
2020-02-08 23:38:25 +0000

    feat: updated routes & some other stuff idk

--- backend/swagger_server/repository/db_client.py
+++ backend/swagger_server/repository/db_client.py
@@ -1,18 +1,45 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from datetime import datetime
 
 cred = credentials.Certificate("swagger_server/firebase_key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-def add_receipt():
+def add_receipt(file_name):
     # example
-    doc_ref = db.collection(u'users').document(u'alovelace')
-    doc_ref.set({
-        u'first': u'Ada',
-        u'last': u'Lovelace',
-        u'born': 1815
-    })
-    return
+    doc_ref = db.collection(u'receipts').document()
+    doc_dict = {
+        u'VAT': 30,
+        u'currency': u'EUR',
+        u'date': datetime.now(),
+        u'details': [
+            {
+                "item": "fries",
+                "value": 40
+            },
+            {
+                "item": "redbull",
+                "value": 60
+            }
+        ],
+        u'emitter': u'Waitrose',
+        u'image_filename': file_name,
+        u'receipt_holder': u'35294866593545759',
+        u'receiver': u'95191861583545753',
+        u'total': 30
+    }
+    doc_ref.set(doc_dict)
+    return doc_dict
+
+
+def get_receipt(id):
+    doc_ref = db.collection(u'receipts').document(f'{id}')
+    return doc_ref.get().to_dict()
+
+
+def get_all_receipts():
+    doc_ref = db.collection(u'receipts').get().to_dict()
+    return doc_ref
--- backend/swagger_server/service/receipt_management.py
+++ backend/swagger_server/service/receipt_management.py
@@ -2,13 +2,22 @@ import uuid
 import base64
 import os
 from flask import send_from_directory
+from swagger_server.repository.db_client import get_receipt
+from swagger_server.repository import db_client
 
 
-def add_receipt_storage(file):
-    file_name = f'receipt-{uuid.uuid1()}'
+def add_receipt_storage(request):
+    file = request.files['file']
+    file_name = f'receipt-{uuid.uuid1()}.png'
     file.save(os.path.join('/tmp/uploads', file_name))
-    return file_name
+    doc_dict = db_client.add_receipt(file_name)
+    return doc_dict
 
 
-def get_receipt_from_storage(file_name):
-    return send_from_directory('/tmp/uploads', file_name)
+def get_receipt_from_storage(id):
+    receipt = get_receipt(id)
+    return send_from_directory('/tmp/uploads', receipt["image_filename"])
+
+
+def get_all_receipts():
+    return db_client.get_all_receipts()

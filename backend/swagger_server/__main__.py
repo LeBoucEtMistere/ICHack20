056870c14e75a4637de1889f97d524358dc0f9aa
Depasse Arthur <arthur.depasse96@gmail.com>
2020-02-09 10:32:24 +0000

    fixng cors

--- backend/swagger_server/__main__.py
+++ backend/swagger_server/__main__.py
@@ -15,7 +15,7 @@ firebase_admin.initialize_app(cred)
 app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={'title': 'Simple Inventory API'})
-CORS(app)
+CORS(app.app)
 
 
 # @app.app.after_request

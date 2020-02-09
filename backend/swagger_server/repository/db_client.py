5a93b904c305df5102dc57fd89fd0d7e014a5333
Depasse Arthur <arthur.depasse96@gmail.com>
2020-02-09 01:13:35 +0000

    adding ocr

--- backend/swagger_server/repository/db_client.py
+++ backend/swagger_server/repository/db_client.py
@@ -2,6 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from datetime import datetime
+from swagger_server.service.ocr import process_image
+import os
+
 
 cred = credentials.Certificate("swagger_server/firebase_key.json")
 firebase_admin.initialize_app(cred)
@@ -10,26 +13,23 @@ db = firestore.client()
 
 def add_receipt(file_name):
     # example
+    tally1, tally2 = process_image(os.path.join('/tmp/uploads', file_name))
     doc_ref = db.collection(u'receipts').document()
     doc_dict = {
-        u'VAT': 30,
-        u'currency': u'EUR',
+        u'VAT': 20,
+        u'currency': tally2["currency"],
         u'date': datetime.now(),
         u'details': [
             {
-                "item": "fries",
-                "value": 40
-            },
-            {
-                "item": "redbull",
-                "value": 60
-            }
+                "item": key,
+                "value": value
+            } for key, value in tally1.items()
         ],
-        u'emitter': u'Waitrose',
+        u'emitter': tally2["store"],
         u'image_filename': file_name,
         u'receipt_holder': u'35294866593545759',
         u'receiver': u'95191861583545753',
-        u'total': 30,
+        u'total': tally2["total"],
         u'validated': False
     }
     doc_ref.set(doc_dict)
